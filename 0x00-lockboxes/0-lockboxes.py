61bea023ccbbb12fe23483b2c4a634172db78bac
CutiePizza <ines.chokri97@gmail.com>
2020-07-18 12:20:16 +0100

    pep8 fixed

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,22 +2,24 @@
 """
 lockboxes problem
 """
-def canUnlockAll(boxes):
-	myDict = {}
-	key_list = []
-	myDict[0] = True
-	key_list.append(boxes[0])
 
-	for i in key_list:
-		for j in i:
-			if j not in myDict:
-				myDict[j] = True
-				i.remove(j)
-			else:
-				i.remove(j)
-			key_list.append(boxes[j])
-	if len(myDict) != len(boxes):
-		return (False)
-	else:
-		return (True)
-	
+
+def canUnlockAll(boxes):
+    """lockboxes"""
+    myDict = {}
+    key_list = []
+    myDict[0] = True
+    key_list.append(boxes[0])
+
+    for i in key_list:
+        for j in i:
+            if j not in myDict:
+                myDict[j] = True
+                i.remove(j)
+            else:
+                i.remove(j)
+                key_list.append(boxes[j])
+    if len(myDict) != len(boxes):
+        return (False)
+    else:
+        return (True)
