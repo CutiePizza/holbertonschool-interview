0728995b073876f69df73c613f690513af74a996
CutiePizza <ines.chokri97@gmail.com>
2020-07-26 16:42:28 +0100

    fix

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -20,6 +20,12 @@ listint_t *insert_node(listint_t **head, int number)
 	new_node->next = NULL;
 	if (*head == NULL)
 		*head = new_node;
+	else if ((*head)->n > number)
+	{
+		new_node->next = *head;
+		*head = new_node;
+
+	}
 	else
 	{
 		p = *head;
