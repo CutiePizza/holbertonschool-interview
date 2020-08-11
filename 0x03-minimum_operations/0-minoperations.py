21d5e5b31437eb5c4aed8c730635db83928e9169
CutiePizza <ines.chokri97@gmail.com>
2020-08-11 20:08:55 -0300

    case n is 0

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,11 +1,10 @@
-#!/usr/bin/python3
 """
 Interview task
 """
 
 
 def minOperations(n):
-    if n == 1:
+    if n == 1 or n == 0:
         return 0
     elif n == 2:
         return 2
@@ -14,3 +13,11 @@ def minOperations(n):
             if n % i == 0:
                 op = n // i
                 return op + minOperations(i)
+
+if __name__ == "__main__":
+    n = 4
+    print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+    n = 0
+    print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+    
