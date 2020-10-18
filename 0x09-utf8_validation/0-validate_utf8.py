89c3f6a66ac3ae38ca1fbf7344686bb63e158528
CutiePizza <ines.chokri97@gmail.com>
2020-10-18 19:44:15 -0300

    Task utf8 validation

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -3,23 +3,23 @@
 Utf8 validation module
 """
 
+
 def validUTF8(data):
     """
     Check if data is a validate utf8
     """
-
-    n_bytes = 0 
+    n_bytes = 0
     for num in data:
         num = bin(num)[2:].zfill(8)
-        if n_bytes: 
+        if n_bytes:
             if num.startswith("10"):
                 n_bytes -= 1
             else:
-                return False 
+                return False
         else:
             n_bytes = num.find("0")
             if n_bytes == -1 or n_bytes == 1 or n_bytes > 4:
-                return False 
+                return False
             if n_bytes:
                 n_bytes -= 1
-    return n_bytes == 0
+    return (n_bytes == 0)
