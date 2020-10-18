d69dde8be54b7b1c323fb667a47575f128a2919d
CutiePizza <ines.chokri97@gmail.com>
2020-10-18 18:31:03 -0300

    Cretaion of tree

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * binary_tree_node - Create a binary tree
+ * @parent: Parent node of the node to create
+ * @value: value of node to create
+ * Return: new node
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+   binary_tree_t *tree;
+
+   tree = malloc(sizeof(binary_tree_t));
+   if (tree == NULL)
+   {
+       return (NULL);
+   }
+   tree->n = value;
+   tree->parent = parent;
+   tree->left = NULL;
+   tree->right = NULL;
+   return (tree);
+}
