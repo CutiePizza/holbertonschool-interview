5ae848e53e9277e7c0b3ac244dec251dd0d755bf
CutiePizza <ines.chokri97@gmail.com>
2020-11-28 18:48:57 -0300

    add if statement

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -2,24 +2,27 @@
 const request = require('request');
 const id = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + id;
+
 request(url, { json: true }, (err, res, body) => {
-	if (err) { return console.log(err); }
-	let names = [];
-	let ids = [];
-	body.characters.forEach(element => {
-		let id = element.split('/')[5];
-		ids.push(id);
-		request(element, {json: true}, (err, res, body) => {
-			if (err) { return console.log(err); }
-			names[id] = body.name;
-		});  
-	});
-	setTimeout(function() {
-		let j;
-		for (i = 0; i < ids.length; i++)
-		{
-			j = ids[i];
-			console.log(names[j]);
-		}
-	}, 1000)
+  if (err) { return console.log(err); }
+  let names = [];
+  let ids = [];
+  body.characters.forEach(element => {
+    let id = element.split('/')[5];
+    ids.push(id);
+    request(element, {json: true}, (err, res, body) => {
+      if (err) { return console.log(err); }
+        if (res.statusCode === 200)
+          names[id] = body.name;
+      });  
+});
+
+setTimeout(function() {
+  let j;
+  for (i = 0; i < ids.length; i++)
+  {
+    j = ids[i];
+    console.log(names[j]);
+  }
+}, 1000)
 });
