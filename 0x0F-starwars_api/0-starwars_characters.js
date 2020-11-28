5efde77c77a55e6b2ee8b5a7e88a4c02f8bd5fff
CutiePizza <ines.chokri97@gmail.com>
2020-11-28 19:50:13 -0300

    permission

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -6,10 +6,8 @@ const url = 'https://swapi-api.hbtn.io/api/films/' + id;
 request(url, { json: true }, (err, res, body) => {
 	if (err) { return console.log(err); }
 	let names = [];
-	let ids = [];
 	body.characters.forEach(element => {
 		let id = element.split('/')[5];
-		ids.push(id);
 		request(element, {json: true}, (err, res, body) => {
 			if (err) { return console.log(err); }
 			if (res.statusCode === 200)
