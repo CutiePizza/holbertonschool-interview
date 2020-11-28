4120c35a1d50dbe3ae513064cf1e6945b890d0d6
CutiePizza <ines.chokri97@gmail.com>
2020-11-28 19:46:36 -0300

    fix

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -4,23 +4,24 @@ const id = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + id;
 
 request(url, { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); }
-  let names = [];
-  let ids = [];
-  body.characters.forEach(element => {
-    let id = element.split('/')[5];
-    ids.push(id);
-    request(element, {json: true}, (err, res, body) => {
-      if (err) { return console.log(err); }
-        if (res.statusCode === 200)
-          names[id] = body.name;
-      });  
+	if (err) { return console.log(err); }
+	let names = [];
+	let ids = [];
+	body.characters.forEach(element => {
+		let id = element.split('/')[5];
+		ids.push(id);
+		request(element, {json: true}, (err, res, body) => {
+			if (err) { return console.log(err); }
+			if (res.statusCode === 200)
+				names[id] = body.name;
+		});  
+	});
+
+	setTimeout(function() {
+		body.characters.forEach(element => {
+			let id = element.split('/')[5];
+			console.log(names[id]);
+		});
+	}, 2000);
 });
 
-setTimeout(function() {
-  body.characters.forEach(element => {
-    let id = element.split('/')[5];
-    console.log(names[id]);
-  });
-}, 2000);
-});
