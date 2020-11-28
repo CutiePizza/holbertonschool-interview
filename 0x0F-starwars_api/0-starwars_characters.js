4758797bb1e22a58a54348c33d22a54484916423
CutiePizza <ines.chokri97@gmail.com>
2020-11-28 18:30:46 -0300

    task api

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const request = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + id;
+request(url, { json: true }, (err, res, body) => {
+	if (err) { return console.log(err); }
+	let names = [];
+	let ids = [];
+	body.characters.forEach(element => {
+		let id = element.split('/')[5];
+		ids.push(id);
+		request(element, {json: true}, (err, res, body) => {
+			if (err) { return console.log(err); }
+			names[id] = body.name;
+		});  
+	});
+	setTimeout(function() {
+		let j;
+		for (i = 0; i < ids.length; i++)
+		{
+			j = ids[i];
+			console.log(names[j]);
+		}
+	}, 1000)
+});
