278a977666d193b3336d33904b8973ba30fe5d7a
CutiePizza <ines.chokri97@gmail.com>
2020-12-20 18:42:23 -0300

    fixed pep8 warnings

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -2,17 +2,19 @@
 """
 Rain task
 """
+
+
 def rain(walls):
     """
     Method that calculates how much water can it retain
     """
     s = 0
-    for i in range(1, len(walls) - 1) : 
+    for i in range(1, len(walls) - 1):
         left = walls[i]
-        for j in range(i) :
-            left = max(left, walls[j]);  
-        right = walls[i]   
-        for j in range(i + 1 , len(walls)) : 
+        for j in range(i):
+            left = max(left, walls[j])
+        right = walls[i]
+        for j in range(i + 1, len(walls)):
             right = max(right, walls[j])
         s = s + (min(left, right) - walls[i])
     return s
