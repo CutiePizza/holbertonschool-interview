826ea8ad14de8e32db985428bdf8f307abcf4637
CutiePizza <ines.chokri97@gmail.com>
2020-12-22 15:29:46 -0300

    fix type of i in heap sort

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -52,7 +52,7 @@ void heapify(int *array, size_t n, size_t i, size_t size)
 
 void heap_sort(int *array, size_t size)
 {
-	int i;
+	size_t i;
 
 	for (i = size / 2 - 1; i >= 0; i--)
 		heapify(array, size, i, size);
