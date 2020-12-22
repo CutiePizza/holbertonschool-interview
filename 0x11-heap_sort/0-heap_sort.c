b950e493042819a91b7d5b86aa124540c4ec18c9
CutiePizza <ines.chokri97@gmail.com>
2020-12-22 14:49:41 -0300

    heap sort

---
+++ 0x11-heap_sort/0-heap_sort.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * swap - swap two cases
+ * @a: integer
+ * @b: integer
+ */
+
+void swap(int *a, int *b)
+{
+	int tmp = *a;
+
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * heapify - Heapify an array
+ * @array: array of integers to heapify
+ * @n: size_t
+ * @i: size_t
+ * @size: size_t
+ */
+
+void heapify(int *array, size_t n, size_t i, size_t size)
+{
+	size_t largest = i;
+	size_t left = 2 * i + 1;
+	size_t right = 2 * i + 2;
+
+	if (left < n && array[left] > array[largest])
+		largest = left;
+
+	if (right < n && array[right] > array[largest])
+		largest = right;
+
+	if (largest != i) //if root not largest
+	{
+		swap(&array[i], &array[largest]);
+		print_array(array, size);
+		heapify(array, n, largest, size);
+	}
+}
+
+/**
+ * heap_sort - heap sort array
+ * @array: array of intgers
+ * @size: size_t of array
+ */
+
+void heap_sort(int *array, size_t size)
+{
+	int i;
+
+	for (i = size / 2 - 1; i >= 0; i--)
+		heapify(array, size, i, size);
+	for (i = size - 1; i >= 0; i--)
+	{
+		swap(&array[0], &array[i]);
+		print_array(array, size);
+	}
+	heapify(array, i, 0, size);
+}
---
+++ 0x11-heap_sort/sort.h
@@ -0,0 +1,5 @@
+#ifndef _SORT_H
+#define _SORT_H
+void heap_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+#endif
