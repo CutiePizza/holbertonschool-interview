be963ac8753fbcaacbf39b26b55f16791db3043a
CutiePizza <ines.chokri97@gmail.com>
2020-12-22 17:28:16 -0300

    fix

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -32,11 +32,9 @@ void heapify(int *array, size_t n, size_t i, size_t size)
 
 	if (left < n && array[left] > array[largest])
 		largest = left;
-
 	if (right < n && array[right] > array[largest])
 		largest = right;
-
-	if (largest != i) //if root not largest
+	if (largest != i)
 	{
 		swap(&array[i], &array[largest]);
 		print_array(array, size);
@@ -56,9 +54,9 @@ void heap_sort(int *array, size_t size)
 
 	if (size <= 2)
 		return;
-	for (i = size / 2 - 1; i >= 0; i--)
+	for (i = size / 2 - 1; (int)i >= 0; i--)
 		heapify(array, size, i, size);
-	for (i = size - 1; i >= 0; i--)
+	for (i = size - 1; (int)i >= 0; i--)
 	{
 		swap(&array[0], &array[i]);
 		print_array(array, size);
