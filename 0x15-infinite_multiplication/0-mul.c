4e220e1efe134a63bcdf2ea6d3c64a606caa650a
CutiePizza <ines.chokri97@gmail.com>
2021-01-19 21:56:33 -0300

    fix betty

--- 0x15-infinite_multiplication/0-mul.c
+++ 0x15-infinite_multiplication/0-mul.c
@@ -10,10 +10,10 @@
  */ 
 int len(char *ch)
 {
-  int i = 0;
-  while (ch[i] != '\0')
-    i++;
-  return (i);
+    int i = 0;
+    while (ch[i] != '\0')
+        i++;
+    return (i);
 }
 /**
  * verify_num - verify if num is a number
@@ -21,18 +21,18 @@ int len(char *ch)
  */
 void verify_num(char *ch)
 {
-  int i;
+    int i;
   
-  for (i = 0; i < len(ch); i++)
-  {
-    if (ch[i] >= '0' && ch[i] <= '9')
-      continue;
-    else
+    for (i = 0; i < len(ch); i++)
     {
-      printf("Error\n");
-      exit(98);
+        if (ch[i] >= '0' && ch[i] <= '9')
+            continue;
+        else
+        {
+            printf("Error\n");
+            exit(98);
+        }
     }
-  }
 }
 /**
  * main - main function
@@ -42,25 +42,25 @@ void verify_num(char *ch)
  */
 int main(int argc, char *argv[])
 {
-  int len1, len2, i, j;
-  int *result;
-  int c = 0;
-  int nonzero = 0;
-  int carry;
-  static char *str;
+    int len1, len2, i, j;
+    int *result;
+    int c = 0;
+    int nonzero = 0;
+    int carry;
+    static char *str;
 
-  if (argc - 1 != 2)
-  {
-    printf("Error\n");
-    exit(98);
-  }
-  verify_num(argv[1]);
-  verify_num(argv[2]);
-  len1 = len(argv[1]);
-  len2 = len(argv[2]);
-  result = malloc(sizeof(int) * (len1 + len2));
-  for (i = len1 - 1; i >= 0; i--)
-  {
+    if (argc - 1 != 2)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+    verify_num(argv[1]);
+    verify_num(argv[2]);
+    len1 = len(argv[1]);
+    len2 = len(argv[2]);
+    result = malloc(sizeof(int) * (len1 + len2));
+    for (i = len1 - 1; i >= 0; i--)
+    {
         c = len1 - 1 - i;
         carry = 0;
         for (j = len2 - 1; j>=0; j--, c++)
