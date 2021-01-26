d951f80528bc3ca682b83e83c134ea0c7e019058
CutiePizza <ines.chokri97@gmail.com>
2021-01-25 21:22:51 -0300

    task find loop

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - find if a list has a loop
+ * @head : head of the list
+ * Return: a node
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow = head, *fast = head;
+
+	if (head == NULL)
+		return (NULL);
+	while (slow && fast && fast->next) {
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			break;
+	}
+	slow = head;
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next;
+		if (slow == fast) {
+			return(slow);
+		}
+	}
+	return (NULL);
+}
