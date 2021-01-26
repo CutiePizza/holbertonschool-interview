8f9f2d262739e0c9ed4c6cfb844bc2810f863aa0
CutiePizza <ines.chokri97@gmail.com>
2021-01-26 19:50:01 -0300

    fix

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -20,7 +20,7 @@ listint_t *find_listint_loop(listint_t *head)
 	if (slow == fast)
 	{
 		slow = head;
-		while (fast && fast->next && fast != slow)
+		while (fast != slow)
 		{
 			slow = slow->next;
 			fast = fast->next;
