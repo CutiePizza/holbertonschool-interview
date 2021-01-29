7aa42cec77805dffa51e55416abda537e0c9adf1
CutiePizza <ines.chokri97@gmail.com>
2021-01-29 16:10:12 -0300

    fix betty

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -1,10 +1,11 @@
 #include "search_algos.h"
 
 /**
-* print_array - print array
-* @start: Where to start to print
-* @end: end of printing
-*/
+ * print_array - print array
+ * @array: array
+ * @start: Where to start to print
+ * @end: end of printing
+ */
 void print_array(int *array, int start, int end)
 {
 	int i;
@@ -19,13 +20,13 @@ void print_array(int *array, int start, int end)
 }
 
 /**
-* binary - Recursive algorithm for binary search
-* @array: array
-* @l: left of array
-* @r: right of array
-* @value: value to look for
-* Return: Index of located value or -1 if not found
-*/
+ * binary - Recursive algorithm for binary search
+ * @array: array
+ * @l: left of array
+ * @r: right of array
+ * @value: value to look for
+ * Return: Index of located value or -1 if not found
+ */
 
 int binary(int *array, int l, int r, int value)
 {
@@ -37,23 +38,23 @@ int binary(int *array, int l, int r, int value)
 
 		if (array[m] ==  value && l == r)
 			return (m);
-        print_array(array, l, r);
+		print_array(array, l, r);
 		if (array[m] >= value)
-			return binary(array, l, m, value);
-		return binary(array, m + 1, r, value);
+			return (binary(array, l, m, value));
+		return (binary(array, m + 1, r, value));
 	}
 	return (-1);
 }
 /**
-* advanced_binary - Search for a given integer
-* @array: array of integer
-* @size: size or array
-* @value: value to look for
-* Return: Index of value
-*/
+ * advanced_binary - Search for a given integer
+ * @array: array of integer
+ * @size: size or array
+ * @value: value to look for
+ * Return: Index of value
+ */
 int advanced_binary(int *array, size_t size, int value)
 {
-    if (!array || size < 1)
-        return (-1);
-    return (binary(array, 0, (int)size - 1, value));
+	if (!array || size < 1)
+		return (-1);
+	return (binary(array, 0, (int)size - 1, value));
 }
