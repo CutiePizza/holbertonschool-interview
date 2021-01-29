1749ae861f6e43845d230773031e9e3e0871b5e0
CutiePizza <ines.chokri97@gmail.com>
2021-01-29 20:59:11 -0300

    fix

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -16,31 +16,28 @@ void swap(int *a, int *b)
 
 	*a = *b;
 	*b = tmp;
-	
+
 }
-
 /**
- * heapify - Heapify an array
- * @array: array of integers to heapify
- * @n: size_t
- * @i: size_t
- * @size: size_t
+ * heapify - heapify array
+ * @n: integer
+ * @i: integer
+ * @size: size of array
  */
-
-void heapify(int *array, size_t n, size_t i, size_t size)
+void heapify(int *array, int n, int i, size_t size)
 {
-	size_t largest = i;
-	size_t left = 2 * i + 1;
-	size_t right = 2 * i + 2;
+	int largest = i;
+	int l = 2 * i + 1;
+	int r = 2 * i + 2;
 
-	if (left < n && array[left] > array[largest])
-		largest = left;
-	if (right < n && array[right] > array[largest])
-		largest = right;
+	if (l < n && array[l] > array[largest])
+		largest = l;
+	if (r < n && array[r] > array[largest])
+		largest = r;
 	if (largest != i)
 	{
 		swap(&array[i], &array[largest]);
-    print_array(array, size);
+		print_array(array, size);
 		heapify(array, n, largest, size);
 	}
 }
@@ -53,16 +50,12 @@ void heapify(int *array, size_t n, size_t i, size_t size)
 
 void heap_sort(int *array, size_t size)
 {
-	size_t i;
-
-	if (!array || size < 2)
-		return;
-	for (i = size / 2 - 1; (int)i >= 0; i--)
-		heapify(array, size, i, size);
-	for (i = size - 1; (int)i >= 0; i--)
+	for (int i = (int)size / 2 - 1; i >= 0; i--)
+		heapify(array, (int)size, i, size);
+	for (int i = (int)size - 1; i > 0; i--)
 	{
-    print_array(array, size);
 		swap(&array[0], &array[i]);
 		heapify(array, i, 0, size);
+		print_array(array, size);
 	}
 }
