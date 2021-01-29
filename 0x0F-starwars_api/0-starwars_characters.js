aa37b6361a32e3ae854dfdf772895570580d2d0a
CutiePizza <ines.chokri97@gmail.com>
2021-01-29 17:01:46 -0300

    adding quotes to request

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -2,7 +2,6 @@
 const request = require('request');
 const id = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + id;
-
 request(url, { json: true }, (err, res, body) => {
   if (err) { return console.log(err); }
   const names = [];
