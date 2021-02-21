1a1268f7a83fda5413c19e8996dcfdcd5d20a463
CutiePizza <ines.chokri97@gmail.com>
2021-02-21 18:11:54 -0300

    fix pep8

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -3,25 +3,26 @@
 Making change
 """
 
+
 def makeChange(coins, total):
-    """determine the fewest number of coins needed to meet a given amount total
-  """
-  if not coins:
-      return (-1)
-  if total in coins:
-      return (1)
-  coins.sort()
-  if (total < coins[0]):
-      return (-1)
-  n = 0
-  i = 0
-  j = len(coins) - 1
-  while (n < total and j >= 0):
-      if (n + coins[j] > total):
-          j = j - 1
-    else:
-        n = n + coins[j]
-      i = i + 1
-  if (n == total):
-      return (i)
-  return (-1)
+    """determine the fewest number of coins
+    """
+    if not coins:
+        return (-1)
+    if total in coins:
+        return (1)
+    coins.sort()
+    if (total < coins[0]):
+        return (-1)
+    n = 0
+    i = 0
+    j = len(coins) - 1
+    while (n < total and j >= 0):
+        if (n + coins[j] > total):
+            j = j - 1
+        else:
+            n = n + coins[j]
+            i = i + 1
+    if (n == total):
+        return (i)
+    return (-1)
