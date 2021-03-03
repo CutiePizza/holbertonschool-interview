74d68fa495cd9979d3c11d2d04649d762f5a45b7
CutiePizza <ines.chokri97@gmail.com>
2021-03-03 16:44:29 -0300

    added condition if string null

--- 0x1A-double_circular_linked_list/0-add_node.c
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -12,7 +12,7 @@ List *add_node_end(List **list, char *str)
 	List *new_node = NULL;
 	List *tmp;
 
-  if (!list)
+  if (!list || !str)
     return (NULL);
 	new_node = malloc(sizeof(List));
 	if (!new_node)
@@ -48,7 +48,7 @@ List *add_node_begin(List **list, char *str)
 {
 	List *new_node = NULL;
 
-  if (!list)
+  if (!list || !str)
     return (NULL);
 	new_node = malloc(sizeof(List));
 	if (!new_node)
