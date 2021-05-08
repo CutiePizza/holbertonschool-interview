191c0daa8226eed460649c9630203b455495a751
CutiePizza <ines.chokri97@gmail.com>
2021-05-08 18:52:57 -0300

    fix

--- 0x1B-radix_sort/0-radix_sort.c
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -57,9 +57,9 @@ void radix_sort(int *array, size_t size)
 	int exp;
 	int m;
 
-    if (!array || size < 2)
-        return;
-    m = getMax(array, size);
+	if (!array || size < 2)
+		return;
+	m = getMax(array, size);
 	for (exp = 1; m / exp > 0; exp *= 10)
 		countSort(array, size, exp);
 }
