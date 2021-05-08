c40ce21d872b83b53037fb2116fcad1c29a28e47
CutiePizza <ines.chokri97@gmail.com>
2021-05-08 18:17:34 -0300

    radix sprt

---
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+/**
+ * getMax - get max int
+ * @arr: array
+ * @n: size of array
+ * Return: max int
+ */
+int getMax(int arr[], size_t n)
+{
+	int mx = arr[0];
+	size_t i;
+
+	for (i = 1; i < n; i++)
+		if (arr[i] > mx)
+			mx = arr[i];
+	return (mx);
+}
+/**
+ * countSort - count sort
+ * @arr: array
+ * @n:size array
+ * @exp: current digit
+ */
+void countSort(int arr[], size_t n, int exp)
+{
+	int output[n];
+	int i, count[10] = { 0 };
+
+	for (i = 0; i < (int)n; i++)
+		count[(arr[i] / exp) % 10]++;
+
+	for (i = 1; i < 10; i++)
+		count[i] += count[i - 1];
+
+	for (i = n - 1; i >= 0; i--)
+	{
+		output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+		count[(arr[i] / exp) % 10]--;
+	}
+	for (i = 0; i < (int)n; i++)
+		arr[i] = output[i];
+	print_array(arr, n);
+}
+/**
+ * radix_sort - radix sort
+ * @array: array
+ * @size: size of array
+ */
+void radix_sort(int *array, size_t size)
+{
+	int exp;
+	int m = getMax(array, size);
+
+	for (exp = 1; m / exp > 0; exp *= 10)
+		countSort(array, size, exp);
+}
---
+++ 0x1B-radix_sort/sort.h
@@ -0,0 +1,6 @@
+#ifndef _RADIX_H_
+#define _RADIX_H_
+void print_array(const int *array, size_t size);
+void radix_sort(int *array, size_t size);
+
+/#endif /* _RADIX_H_ */
