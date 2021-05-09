dfc8979a33183b025c3f3a03fc3a64648bcde494
CutiePizza <ines.chokri97@gmail.com>
2021-05-08 21:46:33 -0300

    task done

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Python3 program for Pascal's Triangle
+"""
+
+
+def next_row(row):
+    """Returns the next row of Pascal's triangle."""
+    n = len(row)
+    new_row = [row[0]] + [row[i] + row[i+1] for i in range(n - 1)] + [row[-1]]
+    return new_row
+
+
+def pascal_triangle(n):
+    """
+    return pascals triangle
+    """
+    row1 = [1]
+    triangle = [row1]
+    for i in range(1, n):
+        triangle.append(next_row(triangle[-1]))
+    return triangle
